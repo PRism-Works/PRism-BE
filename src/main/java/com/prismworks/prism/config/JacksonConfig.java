0fc40becea5075707df1f4b89712daa9be996ffd
DHAPARK <pdh4182@gmail.com>
2024-08-07 20:52:57 +0900

    hotfix : fix ai peer riview api , edit objectMapper

--- src/main/java/com/prismworks/prism/config/JacksonConfig.java
+++ src/main/java/com/prismworks/prism/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.prismworks.prism.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         //objectMapper.registerModule(new Hibernate5Module());
         return objectMapper;
     }
