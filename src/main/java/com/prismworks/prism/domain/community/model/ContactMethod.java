4cfb065bfecd9e4171719964dee892b939a7abaf
cocomongg <pjm4142@naver.com>
2024-11-04 01:09:14 +0900

    feat: TK147 - community mock api demo 추가

---
+++ src/main/java/com/prismworks/prism/domain/community/model/ContactMethod.java
@@ -0,0 +1,24 @@
+package com.prismworks.prism.domain.community.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ContactMethod {
+    KAKAO_TALK("kakaoTalk"),
+    EMAIL("email"),
+    GOOGLE_FORM("googleForm");
+
+    private final String value;
+
+    @JsonCreator
+    public static ContactMethod from(String value) {
+        return Arrays.stream(ContactMethod.values())
+                .filter(contactMethod -> contactMethod.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ContactMethod not match")); //todo: custom Exception
+    }
+}
